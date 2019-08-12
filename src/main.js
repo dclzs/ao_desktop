cccb0c927c452cead1f04eb9d20999232dd6ce87
LC\Administrator <2428494233@qq.com>
2019-08-13 00:19:29 +0800

    vue脚手架搭建完成

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
+Vue.config.productionTip = false
+
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="desktop-container" @contextmenu.prevent="showmenu">
+
+   </div>
+</template>
+
+<script>
+
+export default {
+  name: 'desktop',
+  components: {
+  },
+  methods:{
+    showmenu(){
+      alert("显示右键菜单组件");
+    }
+  }
+}
+</script>
+<style lang="stylus" scope>
+html,body,#app
+  height:100%
+.desktop-container
+  width:100%
+  height:100%
+  background:url("/back.jpeg")
+  background-repeat: no-repeat
+  background-size: cover
+  background-position: center 0
+</style>
