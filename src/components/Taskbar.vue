d0ca30f8d8b3806cc28fd920a7606dc08ef40ac0
LC <242849423@qq.com>
2019-08-13 12:28:20 +0800

    datatime组件开发完成

--- src/components/Taskbar.vue
+++ src/components/Taskbar.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="taskbar">
-		<span class="datetime">
-			2019/8/13 10:15:30
-		</span>
-	</div>
+
+		</div>
 </template>
 <script>
 export default {
@@ -12,14 +10,10 @@ export default {
 </script>
 <style lang="stylus" scoped>
 .taskbar
-	height:.8rem
+	height:.7rem
 	width:100%
 	background:rgba(0,0,0,1)
 	position:fixed
 	left:0
 	top:0
-	text-align:center
-	.datetime
-		color:#fff
-		line-height:.8rem
 </style>
